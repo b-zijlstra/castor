d1decebd98ea697b0e4e0421a7338334cef7d393
Bart Zijlstra <b.zijlstra@tue.nl>
2016-05-27 14:40:25 +0200

    Added script for calculating intensities of vibrations

---
+++ castor/classes/class_dipol.py
@@ -0,0 +1,147 @@
+#! /usr/bin/env python
+
+# 
+# class_dipol.py
+# 
+# Author: Bart Zijlstra
+# 
+# (C) Copyright 2016 Inorganic Materials Chemistry
+# 
+# 
+
+#IMPORTS
+import sys
+import math
+import re
+import numpy as np
+
+#MY PATHS
+
+#MY CLASSES
+from dipols.subclass_matrix import Matrix
+from dipols.subclass_frequency import Frequency
+
+class Dipols:
+    """Holds the dipols from calculating the Hessian"""
+    def __init__(self):
+        self.decimals = 6
+        self.spaces = 3
+        self.matrix = None
+        self.finitdiff = None
+    def read(self, outcar_in):
+        readmode = 0
+        with open(outcar_in, 'r') as inputfile:
+            self.nions = None
+            self.dipol_diff = []
+            self.frequencies = []
+            self.degrees_freedom = []
+            vaspversion=None
+            self.dipol_store=None
+            self.dipol_ref=None
+            for line in inputfile:
+                if(readmode == 0):
+                    #which version of vasp has been used?
+                    match = re.search('^[ \t]*vasp[.]([0-9])[.].*$',line)
+                    if(match): # vasp version found
+                        vaspversion = int(match.group(1))
+                        if(vaspversion != 4 and vaspversion != 5):
+                            print "Error: Unknown Vasp version: " + str(vaspversion)
+                            sys.exit()
+                        readmode += 1
+                        continue
+                elif(readmode == 1):
+                    #read dipole moments
+                    match = re.search('^[ \t]*dipolmoment[ \t]*([0-9.-]+)[ \t]*([0-9.-]+)[ \t]*([0-9.-]+)[ \t]*electrons.*$',line)
+                    # " dipolmoment          -0.000002     -0.051875     -0.352631 electrons x Angstroem"
+                    if(match): # dipol moment found
+                        self.dipol_store = [float(match.group(1)), float(match.group(2)), float(match.group(3))]
+                        if(vaspversion == 4 and self.dipol_ref==None):
+                            self.dipol_ref = [float(match.group(1)), float(match.group(2)), float(match.group(3))]
+                        elif(vaspversion == 4):
+                            dipol = [float(match.group(1)), float(match.group(2)), float(match.group(3))]
+                            self.dipol_diff.append(dipol)
+                        continue
+
+                    #for vasp5, dipols are found for every electronic step, so only use the converged step
+                    if(vaspversion == 5):
+                        match = re.search('^.*aborting loop because EDIFF is reached.*$',line)
+                        # "------------------------ aborting loop because EDIFF is reached ----------------------------------------"
+                        if(match): # electronic convergence
+                            if(self.dipol_ref==None):
+                                self.dipol_ref = self.dipol_store
+                                self.dipol_store = None
+                            else:
+                                self.dipol_diff.append(self.dipol_store)
+                                self.dipol_store = None
+                            continue
+                
+                    #read the degrees of freedom
+                    "              11X         11Y         11Z         12X         12Y         12Z"
+                    match = re.search('^([ \t]+[0-9]+[XYZ])+[ \t]*$',line)
+                    if(match):
+                        self.degrees_freedom = match.group(0).split()
+                        readmode += 1
+                        continue
+                elif(readmode == 2 or readmode == 3):
+                    #make sure we are reading the correct dynamical matrix
+                    if(re.search('^[ \t]*Eigenvectors and eigenvalues of the dynamical matrix[ \t]*$',line)):
+                        readmode += 1
+                        if(vaspversion == 5):
+                            readmode += 1
+                        continue
+                elif(readmode == 4):
+                    match = re.search('^[ \t]*[0-9]+ f  =[ \t]*([0-9.]+) THz[ \t]*([0-9.]+) 2PiTHz[ \t]*([0-9.]+) cm-1[ \t]*([0-9.]+) meV[ \t]*$',line)
+                    # "6 f  =    2.865417 THz    18.003946 2PiTHz   95.580018 cm-1    11.850416 meV"
+                    if(match): # real freq found
+                        self.frequencies.append(Frequency(float(match.group(1)), float(match.group(2)), float(match.group(3)), float(match.group(4)),False))
+                        continue
+                    match = re.search('^[ \t]*[0-9]+ f/i=[ \t]*([0-9.]+) THz[ \t]*([0-9.]+) 2PiTHz[ \t]*([0-9.]+) cm-1[ \t]*([0-9.]+) meV[ \t]*$',line)
+                    if(match): # imag freq found
+                        self.frequencies.append(Frequency(float(match.group(1)), float(match.group(2)), float(match.group(3)), float(match.group(4)),True))
+                        continue
+                    if(len(self.frequencies)>0):
+                        match = re.search('^[ \t]*([0-9.-]+)[ \t]*([0-9.-]+)[ \t]*([0-9.-]+)[ \t]*([0-9.-]+)[ \t]*([0-9.-]+)[ \t]*([0-9.-]+)[ \t]*$',line)
+                        if(match): # reading atomic displacement line
+                            self.frequencies[-1].atdiff.append([float(match.group(4)), float(match.group(5)), float(match.group(6))])
+                            continue
+                        match = re.search('^[ \t]*Finite differences POTIM=[ \t]*([0-9.E-]+)[ \t]*$',line)
+                        # "Finite differences POTIM=  2.00000000000000004E-002"
+                        if(match): # reading POTIM and ending frequency reading
+                            self.finitdiff = float(match.group(1))
+                            readmode += 1 # making sure no more freqs are read
+                            continue
+                elif(readmode == 5):
+                    break
+
+        if(readmode == 0):
+            print "Error: Could not find Vasp version"
+        elif(readmode ==1):
+            print "Error: Could not find dipol moments"
+        elif(readmode ==2 or readmode == 3):
+            print "Error: Could not find dynamical matrix"
+        elif(readmode ==4):
+            print "Error: Could not find all frequencies"
+
+    def getMatrix(self):
+        self.matrix = Matrix()
+        self.matrix.setup(self.dipol_diff,self.degrees_freedom,self.finitdiff)
+        self.matrix.setIntensities(self.frequencies)
+    def writeList3(self, list3, decimals_in = 6, spaces_in = 3):
+        self.decimals = decimals_in
+        self.spaces = spaces_in
+        string = ""
+        xcor = '{0:.{width}f}'.format(list3[0], width=self.decimals)
+        ycor = '{0:.{width}f}'.format(list3[1], width=self.decimals)
+        zcor = '{0:.{width}f}'.format(list3[2], width=self.decimals)
+        string += '{0:>{width}}'.format(xcor, width=self.decimals+self.spaces+4)
+        string += '{0:>{width}}'.format(ycor, width=self.decimals+self.spaces+4)
+        string += '{0:>{width}}'.format(zcor, width=self.decimals+self.spaces+4)
+        return string
+    def write(self,printmode = "all"):
+        if(printmode=="all"):
+            print "Initial dipole moment: " + self.writeList3(self.dipol_ref)
+            self.matrix.printMatrix()
+        count = 0
+        for freq in self.frequencies:
+            count += 1
+            freq.write(prefix=count, printmode=printmode)
---
+++ castor/classes/dipols/subclass_matrix.py
@@ -0,0 +1,88 @@
+#! /usr/bin/env python
+
+# 
+# subclass_matrix.py
+# 
+# Author: Bart Zijlstra
+# 
+# (C) Copyright 2016 Inorganic Materials Chemistry
+# 
+# 
+
+#IMPORTS
+import sys
+import math
+import numpy as np
+
+#MY PATHS
+
+#MY CLASSES
+from subclass_frequency import Frequency
+
+class Matrix:
+    """Defines the dipol matrix from a Hessian"""
+    def __init__(self):
+        self.decimals = 6
+        self.spaces = 3
+        self.dipols = []
+    def setup(self, dipol_diff, degrees_freedom, diff):
+        #check if the amount of dipols match the degrees of freedom
+        if(len(dipol_diff)!= 2*len(degrees_freedom)):
+            print "Error, amounts of dipols and degrees of freedom do not match!"
+            sys.exit()
+
+        atom = 0
+        direction = 0
+        dipcount = 0
+        for degree in degrees_freedom:
+            atom = int(degree[:-1])
+            direction = degree[-1]
+            #first dipol_diff is +diff, second dipol_diff is -diff
+            mux = (dipol_diff[dipcount][0]-dipol_diff[dipcount+1][0])/(2*diff)
+            muy = (dipol_diff[dipcount][1]-dipol_diff[dipcount+1][1])/(2*diff)
+            muz = (dipol_diff[dipcount][2]-dipol_diff[dipcount+1][2])/(2*diff)
+            self.dipols.append([atom,direction,mux, muy, muz])
+            dipcount += 2
+    def setIntensities(self,frequencies):
+        for freq in frequencies:
+            sumx=0
+            sumy=0
+            sumz=0
+            atcount = 0
+            for atom in freq.atdiff:
+                atcount += 1
+                for dip in self.dipols:
+                    if(dip[0] == atcount):
+                        if(dip[1] == "X"):
+                            sumx+=dip[2]* atom[0]
+                            sumy+=dip[3]* atom[0]
+                            sumz+=dip[4]* atom[0]
+                        elif(dip[1] == "Y"):
+                            sumx+=dip[2]* atom[1]
+                            sumy+=dip[3]* atom[1]
+                            sumz+=dip[4]* atom[1]
+                        elif(dip[1] == "Z"):
+                            sumx+=dip[2]* atom[2]
+                            sumy+=dip[3]* atom[2]
+                            sumz+=dip[4]* atom[2]
+            totaldipol = sumx**2 + sumy**2 + sumz**2
+            freq.intensity = [sumx**2,sumy**2,sumz**2,totaldipol]
+
+    def printMatrix(self):
+        for dipol in self.dipols:
+            print dipol
+
+    def printMatrix(self, decimals_in = 6, spaces_in = 3):
+        self.decimals = decimals_in
+        self.spaces = spaces_in
+        for dipol in self.dipols:
+            string = ""
+            string += '{0:>{width}}'.format(dipol[0], width=self.spaces)
+            string += '{0:>{width}}'.format(dipol[1], width=self.spaces)
+            xcor = '{0:.{width}f}'.format(dipol[2], width=self.decimals)
+            ycor = '{0:.{width}f}'.format(dipol[3], width=self.decimals)
+            zcor = '{0:.{width}f}'.format(dipol[4], width=self.decimals)
+            string += '{0:>{width}}'.format(xcor, width=self.decimals+self.spaces+4)
+            string += '{0:>{width}}'.format(ycor, width=self.decimals+self.spaces+4)
+            string += '{0:>{width}}'.format(zcor, width=self.decimals+self.spaces+4)
+            print string
